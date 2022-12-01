74a6142773d3511e3e734d285fadd5404e14f16f
pjehan <pierre.jehan@gmail.com>
2022-12-01 16:59:58 +0100

    Création d'un template Twig pour la page d'accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DefaultController.php',
-        ]);
+        return $this->render('default/homepage.html.twig');
     }
 }
