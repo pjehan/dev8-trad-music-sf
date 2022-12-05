b0f6a254c20c89258fc0f6003ec920da702d45bd
pjehan <pierre.jehan@gmail.com>
2022-12-05 16:31:20 +0100

    Affichage de la liste des musiciens sur une page dédiée

---
+++ src/Controller/MusicianController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MusicianRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MusicianController extends AbstractController
+{
+    #[Route('/musician', name: 'musician_list')]
+    public function list(MusicianRepository $musicianRepository): Response
+    {
+        return $this->render('musician/index.html.twig', [
+            'musicians' => $musicianRepository->findAll(),
+        ]);
+    }
+}
