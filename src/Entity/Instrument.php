2403494a16e853c084625ae9d971211bdaadca00
pjehan <pierre.jehan@gmail.com>
2022-12-07 11:02:26 +0100

    Ajout d'une validation sur la propriété name de Instrument

--- src/Entity/Instrument.php
+++ src/Entity/Instrument.php
@@ -6,6 +6,7 @@ use App\Repository\InstrumentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InstrumentRepository::class)]
 class Instrument
@@ -16,6 +17,7 @@ class Instrument
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'You need to define the instrument name')] // Vérifier que le nom de l'instrument n'est pas vide sur les formulaires
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: true)]
